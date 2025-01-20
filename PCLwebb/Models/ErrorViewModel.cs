b0fae0861840759e56b02f26042ee7b9b630b72d
William Lagerqvist <william.lagerqvist00@gmail.com>
2025-01-20 20:26:57 +0100

    initial project setup

---
+++ PCLwebb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PCLwebb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
